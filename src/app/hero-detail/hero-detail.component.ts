b196dc9c9f683741f9f8f2ce696bd78473efa40e
Willie Owens <willie.owens@urjanet.com>
2018-05-28 18:21:26 -0400

    Substituted hardcoded array of heroes with in-memory http service. Added ability to add/edit/remove heroes. Fixed up CSS some.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -24,7 +24,14 @@ export class HeroDetailComponent implements OnInit {
 
   getHero(): void {
     const id = +this.route.snapshot.paramMap.get('id');
+    if (id === 0)
+      return;
     this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+  }
+  
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
   }
 
   goBack(): void {
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,12 +11,6 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
 
-  selectedHero: Hero;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
@@ -27,6 +21,20 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes().subscribe(
       heroes => this.heroes = heroes
     );
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
   }
 
 }
